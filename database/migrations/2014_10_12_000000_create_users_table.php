7d072a2edb652b4a0f3948ce425ccc54d709061a
Nermen-Esmaeel <nermenes98@gmail.com>
2023-07-20 17:30:28 -0400

    integrate dashboard

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,18 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username');
+            $table->string('firstname')->nullable();
+            $table->string('lastname')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('country')->nullable();
+            $table->string('postal')->nullable();
+            $table->text('about')->nullable();
+            $table->text('roles_name');
             $table->rememberToken();
             $table->timestamps();
         });
