a3da1ef96e9e86e5bde68bb69708898f1a1a19a3
Alaa <al1996sh.9@gmail.com>
2023-07-23 12:19:02 +0300

    Api

---
+++ app/Http/Controllers/Api/ArticleController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    public function index()
+    {
+        $articles = Article::orderBy('created_at', 'desc')->get();
+        if ($$articles->isEmpty()) {
+            return response()->json(['message' => 'No Articles', 'articles' => []]);
+        }
+
+        return response()->json([
+            'articles' =>  $articles
+        ]);
+    }
+}
