fa8e425bc0c41c3828d68900c6a1eadf02b77358
Alaa <al1996sh.9@gmail.com>
2023-08-18 19:47:07 +0300

    fix bade

--- resources/views/roles/show.blade.php
+++ resources/views/roles/show.blade.php
@@ -11,55 +11,56 @@
                 </div>
 
                 <div class="row justify-content-center mt-4">
-                    <div class="col-lg-6">
+                    <div class="col-lg-10">
                         <div class="card mb-4">
                             <div class="card-header  ">
                                 <div class="row">
-                                    <div class="col-md-12 d-flex align-items-center">
-                                        <h6>{{ $role->name }}</h6>
-                                        <div class="col-md-10 text-end">
-                                            <a href="{{ route('roles.edit', $role->id) }}" class="btn btn-success"
-                                                style=""> Edit</a>
-                                            <form action="{{ route('roles.destroy', $role->id) }}" method="POST"
-                                                class="d-inline">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-danger"
-                                                    onclick="return confirm('Are you sure you want to delete this portfolio?')">Delete</button>
-                                            </form>
-                                        </div>
-
+                                    <div class=" col-md-12 text-end">
+                                        <a href="{{ route('roles.edit', $role->id) }}" class="btn btn-success"
+                                            style=""> Edit</a>
+                                        <form action="{{ route('roles.destroy', $role->id) }}" method="POST"
+                                            class="d-inline">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger"
+                                                onclick="return confirm('Are you sure you want to delete this portfolio?')">Delete</button>
+                                        </form>
                                     </div>
 
-                                </div>
-
-
-                                <div class="card">
-
-                                    <div class="card-body px-4 pt-2">
-                                        <p><strong>Role Name:</strong> {{ $role->name }}</p>
-                                        <p><strong>Permissions Name:</strong>
-
-                                        <ul>
-
-                                            @if (!empty($rolePermissions))
-                                                @foreach ($rolePermissions as $v)
-                                                    <li>{{ $v->name }}</li>
-                                                @endforeach
-                                            @endif
-                                        </ul>
-                                        <a href="{{ route('roles.index') }}" class="btn btn-info"
-                                            style="margin-left: 150px">Back</a>
-                                    </div>
                                 </div>
 
                             </div>
-                        </div>
 
+
+                            <div class="card">
+
+                                <div class="card-body px-4 pt-2">
+                                    <p><strong><u> Details of {{ $role->name }} role: </u></strong></p>
+                                    <p><strong>Role Name:</strong> {{ $role->name }}</p>
+                                    <p><strong>Permissions Name:</strong>
+
+                                    <ul>
+
+                                        @if (!empty($rolePermissions))
+                                            @foreach ($rolePermissions as $v)
+                                                <li>{{ $v->name }}</li>
+                                            @endforeach
+                                        @endif
+                                    </ul>
+                                </div>
+                                <div class=" col-md-11 text-end">
+                                    <a href="{{ route('roles.index') }}" class="btn btn-info"
+                                        style="margin-left: 150px">Back</a>
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
+
                 </div>
             </div>
         </div>
         @include('layouts.footers.auth.footer')
     </div>
+
 @endsection
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
     @include('layouts.navbars.auth.topnav', ['title' => 'Users'])
-
     <div class="card shadow-lg mx-4 card-profile-bottom">
         <div class="card-body p-3">
             <div class="row gx-4">
@@ -10,45 +9,42 @@
                     @include('components.alert')
                 </div>
 
-
                 <div class="row justify-content-center mt-4">
-                    <div class="col-lg-6">
+                    <div class="col-lg-10">
                         <div class="card mb-4">
                             <div class="card-header  ">
                                 <div class="row">
-                                    <div class="col-md-12 d-flex align-items-center">
-                                        <h6>{{ $user->username }}</h6>
-                                        <div class=" col-md-10 text-end">
+                                    <div class=" col-md-12 text-end">
+                                        <form action="{{ route('users.destroy', $user->id) }}" method="POST"
+                                            class="d-inline">
+                                            @csrf
+                                            @method('DELETE')
                                             <a href="{{ route('users.edit', $user->id) }}" class="btn btn-success"
                                                 style=""> Edit</a>
-                                            <form action="{{ route('users.destroy', $user->id) }}" method="POST"
-                                                class="d-inline">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-danger"
-                                                    onclick="return confirm('Are you sure you want to delete this portfolio?')">Delete</button>
-                                            </form>
-                                        </div>
-
+                                            <button type="submit" class="btn btn-danger"
+                                                onclick="return confirm('Are you sure you want to delete this portfolio?')">Delete</button>
+                                        </form>
                                     </div>
-
                                 </div>
 
-
                                 <div class="card">
-
                                     <div class="card-body px-4 pt-2">
+                                        <p><strong><u> Details of {{ $user->username }} user: </u></strong></p>
                                         <p><strong>User Name:</strong> {{ $user->username }}</p>
+                                        <p><strong>First Name:</strong> {{ $user->firstname }}</p>
+                                        <p><strong>Last Name:</strong> {{ $user->lastname }}</p>
                                         <p><strong>Email:</strong> {{ $user->email }}</p>
-                                        <p><strong>Password:</strong> {{ $user->password }}</p>
-                                        <p><strong>Address:</strong> {{ $user->address }}</p>
-                                        <p><strong>Role Name:</strong> {!! Form::select('roles_name[]', $roles, [], ['class' => 'form-control']) !!} </p>
 
+                                        <p><strong>Address:</strong> {{ $user->address ?? '-' }}</p>
+                                        <p><strong>Role Name:</strong> {!! Form::select('roles_name[]', $roles, [], ['class' => 'form-control']) !!} </p>
+                                        <p><strong>Created at:</strong> {{ $user->created_at->format('d M Y - h:m') }}</p>
+                                    </div>
+
+                                    <div class=" col-md-11 text-end">
                                         <a href="{{ route('users.index') }}" class="btn btn-info"
                                             style="margin-left: 150px">Back</a>
                                     </div>
                                 </div>
-
                             </div>
                         </div>
                     </div>
